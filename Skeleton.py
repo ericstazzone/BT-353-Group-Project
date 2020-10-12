e9990e6c8b21e0af27ed2985e7a713939929fcfa
Troy Tomasch <troyrh6@gmail.com>
2020-10-11 21:28:31 -0400

    added a few comments

--- Skeleton.py
+++ Skeleton.py
@@ -1,7 +1,10 @@
 import pygame
+## In order to use pygame, it will need to be installed on your computers
 
 ## We could do it with classes or we could do it with just all functions
 
 ## Example of a class we could use
 class Player:
+## Would utilize this class for player movement and all things related to the player
     pass
+
