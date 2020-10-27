d4a50d5f407988f14e10aa5b53f0affadf54813b
Dennis Salmanowitz <salmanowitzd@gmail.com>
2020-10-27 16:29:24 -0400

    Implemented Player Class

--- Skeleton.py
+++ Skeleton.py
@@ -7,9 +7,24 @@
 import pygame, sys
 from pygame.locals import *
 
-class Player:
+class Player():
 ## Would utilize this class for player movement and all things related to the player
-    pass
+    def __init__(self):
+        image = pygame.image.load('square1.png')
+        image = pygame.transform.scale(image, (50, 50))
+        self.image = image
+        self.x = 0
+        self.y = 0
+        
+    ## Player Movement
+    def movex(self, n):
+        self.x += n
+    def movey(self, n):
+        self.y += n
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+    
 
 ##sprite movement test below
 pygame.init()
@@ -17,9 +32,7 @@ clock = pygame.time.Clock()
 size = 800,600 
 screen = pygame.display.set_mode(size,0,32) #DISPLAY=pygame.display.set_mode((800,600)) #creates the display. width of 800 and length of 600
 pygame.display.set_caption('The Impossible Game')
-x, y = 0,0
-sprite = pygame.image.load('square1.png')
-sprite = pygame.transform.scale(sprite, (50,50))
+player = Player()
 loop = True
 #running the game
 while loop:
@@ -30,20 +43,20 @@ while loop:
     pygame.draw.rect(screen,Color("green"),(725,525,75,75))   #finish 
     pygame.draw.rect(screen,Color("blue"),(75,0,30,300))
     pygame.draw.rect(screen,Color("blue"),(0,400,250,30))
-    screen.blit(sprite,(x,y))
+    player.draw(screen)
     ##event control, move to player class
     for event in pygame.event.get():
         if event.type==QUIT:
             loop = False
     key = pygame.key.get_pressed()
     if key[pygame.K_w] or key[pygame.K_UP]:
-        y -= 5
+        player.movey(-5)
     elif key[pygame.K_a] or key[pygame.K_LEFT]: 
-        x -= 5
+        player.movex(-5)
     if key[pygame.K_s] or key[pygame.K_DOWN]: 
-        y += 5
+        player.movey(5)
     elif key[pygame.K_d] or key[pygame.K_RIGHT]: 
-        x += 5
+        player.movex(5)
     #refresh display
     pygame.display.flip()
     clock.tick(120)
