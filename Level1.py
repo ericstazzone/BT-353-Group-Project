3dd4c94107ea494caf29de507c1c62333135ee10
Eric Stazzone <55301772+ericstazzone@users.noreply.github.com>
2020-11-11 00:32:57 -0500

    organized structure of code itself

--- Level1.py
+++ Level1.py
@@ -1,31 +1,26 @@
-
-## In order to use pygame, it will need to be installed on your computers
-## We could do it with classes or we could do it with just all functions
-## Example of a class we could use
-##class Player:
-
 import pygame, sys
 from pygame.locals import *
 
 class Wall(object):
     def __init__(self,pos):
         self.rect=pygame.Rect(pos[0],pos[1],50,50)
+
 class Key():
     def __init__(self,pos):
         pass
-        #import image here
-        # set the x and y 
-        #update function
+                                                                    #import image here
+                                                                    # set the x and y 
+                                                                    #update function
+
 class Traps():
     def __init__(self,pos):
         pass
-        #do the same thing as Player and Key.
-        #make sure to set the x and y 
-        #update function
+                                                                    #do the same thing as Player and Key.
+                                                                    #make sure to set the x and y 
+                                                                    #update function
        
-
 class Player():
-## Would utilize this class for player movement and all things related to the player
+                                                                    ## Would utilize this class for player movement and all things related to the player
     def __init__(self,pos):
         self.images = []
         image1 = pygame.image.load('images/square1.png')
@@ -36,14 +31,9 @@ class Player():
         self.images.append(image3)
         self.index = 0
         self.image = self.images[self.index]
-        #self.image_b=self.image.get_rect()
-        #self.image = pygame.transform.scale(image, (50, 50))
-        #image = pygame.image.load('square1.png')
-        #self.image = image
         self.x = pos[0]
         self.y = pos[1]
         self.counter = 0
-
 
     def update(self):
         self.index += 1
@@ -56,13 +46,14 @@ class Player():
         self.image = self.images[self.counter]
         self.image = pygame.transform.scale(self.image, (40, 40))
         
-    ## Player Movement
     def movex(self, n):
         if not self.wallCollide(n, 0):
             self.x += n
+
     def movey(self, n):
         if not self.wallCollide(0, n):
             self.y += n
+
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
 
@@ -75,23 +66,13 @@ class Player():
         if r.collidelist(rectlist) !=-1:
             sys.exit()
              
-
-    
-    
-
-##sprite movement test below
 pygame.init()
 clock = pygame.time.Clock()
 size = 1000,750
-screen = pygame.display.set_mode(size,0,32) #DISPLAY=pygame.display.set_mode((800,600)) #creates the display. width of 800 and length of 600
+screen = pygame.display.set_mode(size,0,32)
 pygame.display.set_caption('The Impossible Game')
-
-#setting the level to 1
-currentbox=0
-
-
-#making the boundaries
-boxes = [[
+currentbox=0                                                        #setting the level to 1
+boxes = [[                                                          #making the boundaries
     "WWWWWWWWWWWWWWWW WWW",
     "W   W W WWWWWWWW WWW",
     "W           WWWW WWW",
@@ -127,9 +108,7 @@ boxes = [[
 
     ]]
 
-
-    #looping through the string above to get it set up for drawing. Creating rectangle objects
-def load_box(box):
+def load_box(box):                                                  #looping through the string above to get it set up for drawing. Creating rectangle objects
     walls=[]
     x = y = 0
     for row in boxes[box]:
@@ -143,41 +122,28 @@ def load_box(box):
         x = 0
     return walls,player
 
-
-
-
-
 walls,player=load_box(currentbox)
 
-loop = True
-#running the game
+loop = True                                                         #running the game
 while loop:
-    #making the background white
-    screen.fill(Color("white"))
+    screen.fill(Color("white"))                                     #making the background white
 
-    #looping through the walls created to actually creat the rectangles
-
-    #for the y coordinate when it hits the top of the opening, it transports to the box #1
-    if currentbox==0 and player.x>=800 and player.x<=850 and player.y==0:
+    if currentbox==0 and player.x>=800 and player.x<=850 and player.y==0:#for the y coordinate when it hits the top of the opening, it transports to the box #1
         currentbox=1
         walls,player=load_box(currentbox)
-    
-    #considering the event when player decides go back to box #0
-    #doesn't work if I want to go back and forth
-    if currentbox==1 and player.x>=800 and player.x<=850 and player.y==750:
+        
+    if currentbox==1 and player.x>=800 and player.x<=850 and player.y==750:#considering the event when player decides go back to box #0
         currentbox=0
         walls,player=load_box(currentbox)
         player.x=800
         player.y=30
-    
-    for wall in walls:
+
+    for wall in walls:                                              #looping through the walls created to actually creat the rectangles
         pygame.draw.rect(screen, Color("blue"), wall.rect)
 
-
     player.draw()
-
-    ##event control, move to player class
     player.update()
+
     for event in pygame.event.get():
         if event.type==QUIT:
             loop = False
@@ -190,7 +156,6 @@ while loop:
         player.movey(10)
     elif key[pygame.K_d] or key[pygame.K_RIGHT]: 
         player.movex(10)
-    #refresh display
-    pygame.display.flip()
-    clock.tick(30)
-pygame.quit()
+    pygame.display.flip()                                           #refresh display
+    clock.tick(30)                                                  #fps
+pygame.quit()                                                       
