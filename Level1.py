f90e284a1eb024aaeb8436234fa4d65ac8ff0404
edmondahn <55368636+edmondahn@users.noreply.github.com>
2020-11-12 17:57:32 -0500

    transitions

--- Level1.py
+++ Level1.py
@@ -191,12 +191,34 @@ while loop:
     if currentbox==0 and player.x>=800 and player.x<=850 and player.y==0:#for the y coordinate when it hits the top of the opening, it transports to the box #1
         currentbox=1
         walls,player=load_box(currentbox)
-        
-    if currentbox==1 and player.x>=800 and player.x<=850 and player.y==750:#considering the event when player decides go back to box #0
+    #considering the event when player decides go back to box #0 when in the first box   
+    if currentbox==1 and player.x>=800 and player.x<=850 and player.y==750:
         currentbox=0
         walls,player=load_box(currentbox)
         player.x=800
         player.y=30
+    
+    if currentbox==1 and player.x>=150 and player.x<=200 and player.y==0:
+        currentbox=2
+        walls,player=load_box(currentbox)
+    
+    if currentbox==2 and player.x>=150 and player.x<=200 and player.y==750:
+        currentbox=1
+        walls,player=load_box(currentbox)
+        player.x=150
+        player.y=30
+    
+
+    if currentbox==0 and player.y>=650 and player.y<=700 and player.x==1000:
+        currentbox=3
+        walls,player=load_box(currentbox)
+
+    if currentbox==3 and player.y>=650 and player.y<=700 and player.x<0:
+        currentbox=0
+        walls,player=load_box(currentbox)
+        player.x=950
+        player.y=650
+    
 
     for wall in walls:                                              #looping through the walls created to actually creat the rectangles
         pygame.draw.rect(screen, Color("blue"), wall.rect)
