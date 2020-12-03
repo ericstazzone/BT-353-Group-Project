9c6c9ce8f7104f8ec45fc27dcd29ac0cc98dab9e
edmondahn <55368636+edmondahn@users.noreply.github.com>
2020-12-02 20:49:42 -0500

    implemented restart button at the end

--- Level1.py
+++ Level1.py
@@ -1058,6 +1058,34 @@ while loop:
         walls,player,trap,tiles=load_box(currentbox)
         player.x=150
         player.y=720
+    
+    if currentbox==8 and player.y>=350 and player.y<=400 and player.x==1000:
+        color=(255,255,255)
+        color_light=(170,170,170)
+        color_dark=(100,100,100)
+        width=screen.get_width()
+        height=screen.get_height()
+        smallfont=pygame.font.SysFont('Corbel',35)
+        text=smallfont.render('Restart',True,color)
+        finished=True
+        while finished:
+            for ev in pygame.event.get():
+                if ev.type==pygame.QUIT:
+                    pygame.quit()
+                if ev.type==pygame.MOUSEBUTTONDOWN:
+                    if width/2<=mouse[0]<=width/2+140 and height/2 <= mouse[1]<=height/2+40:
+                        currentbox=0
+                        walls,player,trap,tiles=load_box(currentbox)
+                        finished=False
+                        
+            screen.fill((60,25,60))
+            mouse=pygame.mouse.get_pos()
+            if width/2<=mouse[0]<=width/2+140 and height/2<=mouse[1]<=height/2+40:
+                pygame.draw.rect(screen,color_light,[width/2,height/2,140,40])
+            else:
+                pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
+            screen.blit(text,(width/2+10,height/2))
+            pygame.display.update()
 
     for wall in walls:                                              #looping through the walls created to actually creat the rectangles
         pygame.draw.rect(screen, Color("white"), wall.rect)
